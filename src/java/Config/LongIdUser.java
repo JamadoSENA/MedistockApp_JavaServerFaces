195add406a847a2443de0efe35e2cd1941dfa0b9
JamadoSENA <J.amadosena@gmail.com>
2024-05-02 20:48:36 -0500

    Convertidores JSF v.1.0

--- src/java/Config/LongIdUser.java
+++ src/java/Config/LongIdUser.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Config;
 
-/**
- *
- * @author julia
- */
-public class LongIdUser {
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator("lius")
+public class LongIdUser implements Validator {
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        String cad = (String)value;
+        
+        if(cad.length() < 3 || cad.length() > 10){
+            FacesMessage msj = new FacesMessage(" Id de Usuario no es válido - Personalizado");
+            msj.setSeverity(FacesMessage.SEVERITY_ERROR);
+            
+            throw new ValidatorException(msj);
+        }        
+    }
     
 }
--- src/java/Config/UserBean.java
+++ src/java/Config/UserBean.java
@@ -14,7 +14,6 @@ import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
-import javax.faces.validator.FacesValidator;
 
 /**
  *
